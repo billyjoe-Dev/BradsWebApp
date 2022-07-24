d5f5bc581e9281307bc154f51a25d3d46d81dabf
Bradfield <Bradfield@SKM002>
2022-07-25 01:23:51 +0200

    Add project files.

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Majorsoft.Blazor.Components.Common.JsInterop;
+using PortfolioWebsite.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Register dependencies
+builder.Services.AddJsInteropExtensions();
+
+await builder.Build().RunAsync();
